8424977225eb41ddad02810fb2693ff2d21a542c
David Koenig <david.koenig09@outlook.com>
2023-04-01 21:24:35 +0200

    Added first DB file

--- DTS/DTSEndpoints.ps1
+++ DTS/DTSEndpoints.ps1
@@ -42,12 +42,32 @@ function Get-DTSEndpointPokerGetTable {
         New-Item -Path "$script:_endpoint_config_base_path\$script:_endpoint_config_folder\$script:_endpoint_folder\poker" -ItemType Directory | Out-Null
     }
     
-    Add-PodeRoute -Method Get -Path '/api/v1/dts/poker/gettable' -ScriptBlock {
+    Add-PodeRoute -Method Get -Path '/api/v1/dts/poker/gettable' -ArgumentList @{_endpoint_config_base_path = $script:_endpoint_config_base_path; _endpoint_config_folder = $script:_endpoint_config_folder; _endpoint_folder = $script:_endpoint_folder} -ScriptBlock {
 
         Write-PodeLog -Name "log" -InputObject @{Message="Got incoming request on path /api/v1/dts/poker/gettable"; Component="Get-DTSEndpointPokerGetTable"; Type="Info"}
 
-        $_return = New-Object -Type psobject
-        $_return | Add-Member -MemberType NoteProperty -Name "pokerTableName" -Value "Wurschtsemmel" -Force
-        Write-PodeJsonResponse -Value ($_return | ConvertTo-Json)
+        $_poker_table_name = $WebEvent.Query['name']
+        $_poker_table_password = $WebEvent.Query['password']
+
+        Write-PodeLog -Name "log" -InputObject @{ Message="Requested table name: $_poker_table_name with password $_poker_table_password"; Component="Get-DTSEndpointPokerGetTable"; Type="Info" }
+        
+        $_poker_table_json=$null
+
+        Write-PodeLog -Name "log" -InputObject @{ Message="$_endpoint_config_base_path\$_endpoint_config_folder\$_endpoint_folder\poker\$_poker_table_name.json"; Component="Get-DTSEndpointPokerGetTable"; Type="Info" }
+
+        if(Test-Path ("$_endpoint_config_base_path\$_endpoint_config_folder\$_endpoint_folder\poker\$_poker_table_name.json")) {
+            
+            Write-PodeLog -Name "log" -InputObject @{ Message="Table already exist"; Component="Get-DTSEndpointPokerGetTable"; Type="Info" }
+            $_poker_table_json=(Get-Content -Path $InstanceConfig -Raw) | ConvertFrom-Json
+
+        } else {
+            Write-PodeLog -Name "log" -InputObject @{ Message="Table doesn't exist"; Component="Get-DTSEndpointPokerGetTable"; Type="Info" }
+
+            $_poker_table_obj = New-Object -Type psobject
+            $_poker_table_obj | Add-Member -MemberType NoteProperty -Name "pokerTableName" -Value $_poker_table_name -Force
+            $_poker_table_obj | Add-Member -MemberType NoteProperty -Name "pokerTablePassword" -Value $_poker_table_password -Force
+            $_poker_table_json=Write-PodeJsonResponse -Value ($_return | ConvertTo-Json)
+        }
+        Write-PodeJsonResponse -Value ($_poker_table_json | ConvertTo-Json)
     }
 }
